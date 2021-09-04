9c736e5198bd3a734261cbe7d4da8b5ff26003b6
Matt Ferraro <matthewferraro@Matthews-Air.attlocal.net>
2021-09-04 18:47:31 -0400

    Start bringing in opencascade.js

--- cadmium/src/App.js
+++ cadmium/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import initOpenCascade from "opencascade.js";
+
 
 function App() {
+  React.useEffect(() => {
+    console.log("Loading OpenCascade...");
+    initOpenCascade().then(oc => {
+      // Check out the examples on how to use this library!
+      console.log("OC is loaded");
+    });
+    
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
