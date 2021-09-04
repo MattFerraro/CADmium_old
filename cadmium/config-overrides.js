350f729f5514c1e5b1a3ac60c7ac1f6803cc6b75
Matt Ferraro <matthewferraro@Matthews-Air.attlocal.net>
2021-09-04 17:14:03 -0400

    CRA + rewired

---
+++ cadmium/config-overrides.js
@@ -0,0 +1,16 @@
+module.exports = {
+    webpack: (config) => {
+      config.module.rules.find(k => k.oneOf !== undefined).oneOf.unshift(
+        {
+          test: /\.wasm$/,
+          type: "javascript/auto",
+          loader: "file-loader",
+          options: {
+            name: "static/js/[name].[contenthash:8].[ext]",
+          },
+        }
+      );
+      return config;
+    },
+  };
+  
